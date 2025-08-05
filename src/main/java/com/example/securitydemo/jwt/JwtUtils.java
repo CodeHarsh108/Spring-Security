cec6518291b27a019ff25ff866950ff46a4524ed
Harsh Patil <itsharshhh6@gmail.com>
2025-08-05 08:31:32 +0530

    Getting Jwt from header

--- src/main/java/com/example/securitydemo/jwt/JwtUtils.java
+++ src/main/java/com/example/securitydemo/jwt/JwtUtils.java
@@ -1,4 +1,23 @@
 package com.example.securitydemo.jwt;
+import jakarta.servlet.http.HttpServletRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 
+@Component
 public class JwtUtils {
+    private static final Logger logger = LoggerFactory.getLogger(JwtUtils.class);
+    // Getting Jwt from header
+    public String getJwtFromHeader(HttpServletRequest request){
+        String bearerToken = request.getHeader("Authorization");
+        logger.debug("Authorization Header : {}", bearerToken);
+        if(bearerToken != null && bearerToken.startsWith("Bearer ")){
+            return bearerToken.substring(7);
+        }
+        return null;
+    }
+    // Generating token from username
+    // Getting Username from JWT token
+    // Generating signing key
+    // Validate JWT token
 }
