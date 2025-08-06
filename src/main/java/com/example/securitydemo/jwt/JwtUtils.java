3e9537c9a7bf37cc7cde687d13d2508c7ee098b5
Harsh Patil <itsharshhh6@gmail.com>
2025-08-06 06:56:41 +0530

    Generating token from username

--- src/main/java/com/example/securitydemo/jwt/JwtUtils.java
+++ src/main/java/com/example/securitydemo/jwt/JwtUtils.java
@@ -1,12 +1,21 @@
 package com.example.securitydemo.jwt;
+import io.jsonwebtoken.Jwts;
 import jakarta.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
+
+import java.util.Date;
 
 @Component
 public class JwtUtils {
+
     private static final Logger logger = LoggerFactory.getLogger(JwtUtils.class);
+
+    private int jwtExpirationms;
+
+
     // Getting Jwt from header
     public String getJwtFromHeader(HttpServletRequest request){
         String bearerToken = request.getHeader("Authorization");
@@ -16,7 +25,21 @@ public class JwtUtils {
         }
         return null;
     }
+
+
     // Generating token from username
+    public String generateTokenFromUsername(UserDetails userDetails){
+        String username = userDetails.getUsername();
+        return Jwts.builder().
+                subject(username).
+                issuedAt(new Date()).
+                expiration(new Date((new Date().getTime() + jwtExpirationms))).
+                signWith(key()).
+                compact();
+
+    }
+
+
     // Getting Username from JWT token
     // Generating signing key
     // Validate JWT token
